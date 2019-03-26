67d27587fd3b1f75d0a30758814bf28e3b0b6d32
Sultan Alsawaf <sultan@kerneltoast.com>
2019-03-25 23:38:14 -0700

    sysctl: Nuke sched_boost feature
    This is perhaps the worst performance-related feature ever, and is
    thoroughly abused by userspace in all the worst ways possible.
    Triggering a massive task migration via sched_boost during
    latency-critical operations such as app launches is a very bad idea, and
    leads to noticeably-horrible stuttering.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -397,6 +397,7 @@ static struct ctl_table kern_table[] = {
 		.extra1		= &zero,
 		.extra2		= &sysctl_sched_group_upmigrate_pct,
 	},
+#if 0
 	{
 		.procname	= "sched_conservative_pl",
 		.data		= &sysctl_sched_conservative_pl,
@@ -406,6 +407,7 @@ static struct ctl_table kern_table[] = {
 		.extra1		= &zero,
 		.extra2		= &one,
 	},
+#endif
 	{
 		.procname	= "sched_many_wakeup_threshold",
 		.data		= &sysctl_sched_many_wakeup_threshold,
@@ -452,15 +454,6 @@ static struct ctl_table kern_table[] = {
 		.extra2		= &two_million,
 	},
 #endif
-	{
-		.procname	= "sched_boost",
-		.data		= &sysctl_sched_boost,
-		.maxlen		= sizeof(unsigned int),
-		.mode		= 0644,
-		.proc_handler	= sched_boost_handler,
-		.extra1		= &neg_three,
-		.extra2		= &three,
-	},
 	{
 		.procname	= "sched_upmigrate",
 		.data		= &sysctl_sched_capacity_margin_up,
