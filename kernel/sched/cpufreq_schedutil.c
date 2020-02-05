9a1bc85271bd2c4557f15ff6f8d58ca1e6b748e7
Wilson Sung <wilsonsung@google.com>
2020-02-05 14:14:31 +0800

    [PARTIAL] sched: Roll back sched files
    File list:
    kernel/sched/*
    
    Bug: 147204230
    Bug: 146759211
    Change-Id: I683c02c82967654aac2fb97999e021032e9b79ef
    Signed-off-by: Wilson Sung <wilsonsung@google.com>
    Signed-off-by: Volodymyr Zhdanov <wight554@gmail.com>
    [Flopster101: DON'T pick this commit. Get the original here: https://github.com/darkhz/uclamp/commit/0830392dae25cdd6c18356caeff6b71d6c8b45ca]
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -916,7 +916,6 @@ static void sugov_limits(struct cpufreq_policy *policy)
 {
 	struct sugov_policy *sg_policy = policy->governor_data;
 	unsigned long flags;
-	unsigned int ret;
 
 	if (!policy->fast_switch_enabled) {
 		mutex_lock(&sg_policy->work_lock);
@@ -924,10 +923,7 @@ static void sugov_limits(struct cpufreq_policy *policy)
 		mutex_unlock(&sg_policy->work_lock);
 	} else {
 		raw_spin_lock_irqsave(&sg_policy->update_lock, flags);
-		ret = cpufreq_policy_apply_limits_fast(policy);
-		if (ret && policy->cur != ret) {
-			policy->cur = ret;
-		}
+		cpufreq_policy_apply_limits_fast(policy);
 		raw_spin_unlock_irqrestore(&sg_policy->update_lock, flags);
 	}
 
