d02fd050c61f102d6c977d8ccfe33770aedeb9f5
Dyneteve <dyneteve@pixelexperience.org>
2020-03-08 19:38:29 +0200

    [PARTIAL] Revert "sched: Improve the scheduler"
    This reverts commit 2d2b0b196b7036847d25eec77d123b8180abaf49.

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -397,17 +397,6 @@ static struct ctl_table kern_table[] = {
 		.extra1		= &zero,
 		.extra2		= &sysctl_sched_group_upmigrate_pct,
 	},
-#if 0
-	{
-		.procname	= "sched_conservative_pl",
-		.data		= &sysctl_sched_conservative_pl,
-		.maxlen		= sizeof(unsigned int),
-		.mode		= 0644,
-		.proc_handler	= proc_dointvec_minmax,
-		.extra1		= &zero,
-		.extra2		= &one,
-	},
-#endif
 	{
 		.procname	= "sched_many_wakeup_threshold",
 		.data		= &sysctl_sched_many_wakeup_threshold,
