34dcee9524ed51969c45240702519a9474d9a231
Rick Yiu <rickyiu@google.com>
2020-03-25 21:41:10 +0800

    sched: separate capacity margin for boosted tasks
    With the introduction of placement hint patch, boosted tasks will not
    scheduled from big cores. We tune capacity margin to let important
    boosted tasks get scheduled on big cores. However, the capacity margin
    affects all group of tasks, so that non-boosted tasks get more chances
    to be scheduled on big cores, too. This could be solved by separating
    capacity margin for boosted tasks.
    
    Bug: 147785606
    Test: margin set correctly
    Signed-off-by: Rick Yiu <rickyiu@google.com>
    Change-Id: I2b02e138e36a6844afbc1ade60fe86a001814b30

--- include/linux/sched/sysctl.h
+++ include/linux/sched/sysctl.h
@@ -32,6 +32,9 @@ extern unsigned int sysctl_sched_wakeup_granularity;
 extern unsigned int sysctl_sched_child_runs_first;
 extern unsigned int sysctl_sched_capacity_margin_up[MAX_MARGIN_LEVELS];
 extern unsigned int sysctl_sched_capacity_margin_down[MAX_MARGIN_LEVELS];
+extern unsigned int sysctl_sched_capacity_margin_up_boosted[MAX_MARGIN_LEVELS];
+extern unsigned int
+       sysctl_sched_capacity_margin_down_boosted[MAX_MARGIN_LEVELS];
 #ifdef CONFIG_SCHED_WALT
 extern unsigned int sysctl_sched_use_walt_cpu_util;
 extern unsigned int sysctl_sched_use_walt_task_util;
@@ -129,6 +132,10 @@ extern int sysctl_sched_uclamp_handler(struct ctl_table *table, int write,
 #endif
 
 extern int sched_updown_migrate_handler(struct ctl_table *table,
+					int write, void __user *buffer,
+					size_t *lenp, loff_t *ppos);
+
+extern int sched_updown_migrate_handler_boosted(struct ctl_table *table,
 					int write, void __user *buffer,
 					size_t *lenp, loff_t *ppos);
 
