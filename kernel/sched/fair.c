fac11986c281a48786e323496654cf4067b5501c
Diep Quynh <remilia.1505@gmail.com>
2021-08-06 15:48:52 +0700

    sched: Initial sched boost adaption for PELT
    This is made to make use of CAF perf boost daemon when WALT is not
    being compiled with the kernel
    
    Signed-off-by: Diep Quynh <remilia.1505@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6044,15 +6044,17 @@ unsigned long capacity_curr_of(int cpu)
 	return cap_scale(max_cap, scale_freq);
 }
 
-inline bool energy_aware(void)
-{
-	return sysctl_sched_energy_aware;
-}
-
 /*
  * Externally visible function. Let's keep the one above
  * so that the check is inlined/optimized in the sched paths.
  */
+
+bool pelt_energy_aware = true;
+inline bool energy_aware(void)
+{
+	return (likely(pelt_energy_aware)) && sched_feat(ENERGY_AWARE);
+}
+
 bool sched_is_energy_aware(void)
 {
 	return energy_aware();
