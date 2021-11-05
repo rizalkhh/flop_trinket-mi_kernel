409bc832d3c9b967bbb802345b8e890d43632a4b
Fiqri Ardyansyah <fiqri15072019@gmail.com>
2021-11-05 11:51:00 +0700

    usb: Import Xiaomi changes
    * Import from ginkgo-q-oss
    
    Change-Id: Ibd69e3333aa5598908def72c6eab87806c4c1852
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- drivers/usb/gadget/composite.c
+++ drivers/usb/gadget/composite.c
@@ -1815,7 +1815,11 @@ composite_setup(struct usb_gadget *gadget, const struct usb_ctrlrequest *ctrl)
 					cdev->desc.bcdUSB = cpu_to_le16(0x0320);
 					cdev->desc.bMaxPacketSize0 = 9;
 				} else {
+#ifdef CONFIG_MACH_XIAOMI_C3J
+					cdev->desc.bcdUSB = cpu_to_le16(0x0200);
+#else
 					cdev->desc.bcdUSB = cpu_to_le16(0x0210);
+#endif
 				}
 			} else {
 				if (gadget->lpm_capable)
