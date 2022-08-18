2c0152a30a5b19dfe093a8d00c734889f7c59271
Chengming Zhou <zhouchengming@bytedance.com>
2022-08-18 20:48:01 +0800

    sched/fair: Combine detach into dequeue when migrating task
    When we are migrating task out of the CPU, we can combine detach and
    propagation into dequeue_entity() to save the detach_entity_cfs_rq()
    in migrate_task_rq_fair().
    
    This optimization is like combining DO_ATTACH in the enqueue_entity()
    when migrating task to the CPU. So we don't have to traverse the CFS tree
    extra time to do the detach_entity_cfs_rq() -> propagate_entity_cfs_rq(),
    which wouldn't be called anymore with this patch's change.
    
    detach_task()
      deactivate_task()
        dequeue_task_fair()
          for_each_sched_entity(se)
            dequeue_entity()
              update_load_avg() /* (1) */
                detach_entity_load_avg()
    
      set_task_cpu()
        migrate_task_rq_fair()
          detach_entity_cfs_rq() /* (2) */
            update_load_avg();
            detach_entity_load_avg();
            propagate_entity_cfs_rq();
              for_each_sched_entity()
                update_load_avg()
    
    This patch save the detach_entity_cfs_rq() called in (2) by doing
    the detach_entity_load_avg() for a CPU migrating task inside (1)
    (the task being the first se in the loop)
    
    Signed-off-by: Chengming Zhou <zhouchengming@bytedance.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lore.kernel.org/r/20220818124805.601-6-zhouchengming@bytedance.com
    Signed-off-by: Helium-Studio <67852324+Helium-Studio@users.noreply.github.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -3974,6 +3974,7 @@ static void detach_entity_load_avg(struct cfs_rq *cfs_rq, struct sched_entity *s
 #define UPDATE_TG	0x1
 #define SKIP_AGE_LOAD	0x2
 #define DO_ATTACH	0x4
+#define DO_DETACH	0x8
 
 /* Update task and its cfs_rq load average */
 static inline void update_load_avg(struct cfs_rq *cfs_rq, struct sched_entity *se, int flags)
@@ -3996,6 +3997,13 @@ static inline void update_load_avg(struct cfs_rq *cfs_rq, struct sched_entity *s
 		attach_entity_load_avg(cfs_rq, se);
 		update_tg_load_avg(cfs_rq, 0);
 
+	} else if (flags & DO_DETACH) {
+		/*
+		 * DO_DETACH means we're here from dequeue_entity()
+		 * and we are migrating task out of the CPU.
+		 */
+		detach_entity_load_avg(cfs_rq, se);
+		update_tg_load_avg(cfs_rq, 0);
 	} else if (decayed && (flags & UPDATE_TG))
 		update_tg_load_avg(cfs_rq, 0);
 }
@@ -4204,6 +4212,7 @@ done:
 #define UPDATE_TG	0x0
 #define SKIP_AGE_LOAD	0x0
 #define DO_ATTACH	0x0
+#define DO_DETACH	0x0
 
 static inline void update_load_avg(struct cfs_rq *cfs_rq, struct sched_entity *se, int not_used1)
 {
@@ -4530,6 +4539,11 @@ static __always_inline void return_cfs_rq_runtime(struct cfs_rq *cfs_rq);
 static void
 dequeue_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int flags)
 {
+	int action = UPDATE_TG;
+
+	if (entity_is_task(se) && task_on_rq_migrating(task_of(se)))
+		action |= DO_DETACH;
+
 	/*
 	 * Update run-time statistics of the 'current'.
 	 */
@@ -4543,7 +4557,7 @@ dequeue_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int flags)
 	 *   - For group entity, update its weight to reflect the new share
 	 *     of its group cfs_rq.
 	 */
-	update_load_avg(cfs_rq, se, UPDATE_TG);
+        update_load_avg(cfs_rq, se, action);
 	dequeue_runnable_load_avg(cfs_rq, se);
 
 	update_stats_dequeue(cfs_rq, se, flags);
@@ -9073,7 +9087,7 @@ static void migrate_task_rq_fair(struct task_struct *p)
 		lockdep_assert_held(&task_rq(p)->lock);
 		detach_entity_cfs_rq(&p->se);
 
-	} else {
+	} else if (!task_on_rq_migrating(p)) {
 		/*
 		 * We are supposed to update the task to "current" time, then
 		 * its up to date and ready to go to new CPU/cfs_rq. But we
