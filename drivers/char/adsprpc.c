dc9a489aef250e89c70554cfb7606977ccd17ad8
Nahuel Gómez <nahuelgomez329@gmail.com>
2025-03-18 20:09:24 -0300

    drivers: Import misc SoC changes from ginkgo-q-oss
    Signed-off-by: Nahuel Gómez <nahuelgomez329@gmail.com>

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2012-2021, The Linux Foundation. All rights reserved.
+ * Copyright (C) 2021 XiaoMi, Inc.
  * Copyright (c) 2022-2024, Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
