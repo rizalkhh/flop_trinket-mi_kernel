8c78d0c19a21ee0cf9762657b6c543ef270fba55
Flopster101 <nahuelgomez329@gmail.com>
2025-03-23 20:35:00 -0300

    init: only force do_skip_initramfs to 0 in two-stage ROMs
    The way it was done before caused Magisk to not work on legacy ROMs, because it would always set do_skip_initramfs to 1 on them, and Magisk requires the initramfs to be present.
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- init/initramfs.c
+++ init/initramfs.c
@@ -629,11 +629,7 @@ static int __init skip_initramfs_param(char *str)
 {
 	if (*str)
 		return 0;
-
-	if (strstr(saved_command_line, "fstabdt_keep"))
-		do_skip_initramfs = 1;
-	else
-		do_skip_initramfs = !IS_ENABLED(CONFIG_INITRAMFS_IGNORE_SKIP_FLAG);
+	do_skip_initramfs = !IS_ENABLED(CONFIG_INITRAMFS_IGNORE_SKIP_FLAG);
 	
 	return 1;
 }
@@ -642,6 +638,9 @@ __setup("skip_initramfs", skip_initramfs_param);
 static int __init populate_rootfs(void)
 {
 	char *err;
+
+	if (!strstr(saved_command_line, "fstabdt_keep"))
+		do_skip_initramfs = 0;
 
 	if (do_skip_initramfs) {
 		if (initrd_start)
