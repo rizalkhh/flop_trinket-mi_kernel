12754c5ec7934827f60469137b391a3c4255e439
Flopster101 <nahuelgomez329@gmail.com>
2025-03-25 17:55:05 -0300

    proc: cmdline: simplify ignore_skip_flag code
    Just use proc_command_line always.
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -11,17 +11,15 @@
 extern int susfs_spoof_cmdline_or_bootconfig(struct seq_file *m);
 #endif
 
+static char proc_command_line[COMMAND_LINE_SIZE];
+
 #ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
 #define INITRAMFS_STR_FIND "skip_initramf"
 #define INITRAMFS_STR_REPLACE "want_initramf"
 #define INITRAMFS_STR_LEN (sizeof(INITRAMFS_STR_FIND) - 1)
 
-static char proc_command_line[COMMAND_LINE_SIZE];
-
 static void proc_command_line_init(void) {
 	char *offset_addr;
-
-	strcpy(proc_command_line, saved_command_line);
 
 	offset_addr = strstr(proc_command_line, INITRAMFS_STR_FIND);
 	if (!offset_addr)
@@ -39,18 +37,8 @@ static int cmdline_proc_show(struct seq_file *m, void *v)
 		return 0;
 	}
 #endif
-#ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
-	if (strstr(saved_command_line, "fstabdt_keep")) {
-		seq_puts(m, saved_command_line);
-		seq_putc(m, '\n');
-	} else {
-		seq_puts(m, proc_command_line);
-		seq_putc(m, '\n');
-	}
-#else
-	seq_puts(m, saved_command_line);
+	seq_puts(m, proc_command_line);
 	seq_putc(m, '\n');
-#endif
 	return 0;
 }
 
@@ -68,6 +56,8 @@ static const struct file_operations cmdline_proc_fops = {
 
 static int __init proc_cmdline_init(void)
 {
+	strcpy(proc_command_line, saved_command_line);
+
 #ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
 	if (!strstr(saved_command_line, "fstabdt_keep"))
 		proc_command_line_init();
