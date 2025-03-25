a9193cf6dcd3096dcffd59bfefa38ff4afe53508
Flopster101 <nahuelgomez329@gmail.com>
2025-03-25 04:30:26 -0300

    drivers: zram: Add init protection for compression algorithm
    This will prevent ROMs from changing the algorithm on boot.
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -33,6 +33,7 @@
 #include <linux/sysfs.h>
 #include <linux/debugfs.h>
 #include <linux/cpuhotplug.h>
+#include <linux/binfmts.h>
 
 #include "zram_drv.h"
 
@@ -1002,6 +1003,9 @@ static ssize_t comp_algorithm_store(struct device *dev,
 	struct zram *zram = dev_to_zram(dev);
 	char compressor[ARRAY_SIZE(zram->compressor)];
 	size_t sz;
+
+	if (task_is_booster(current))
+		return -EPERM;
 
 	strlcpy(compressor, buf, sizeof(compressor));
 	/* ignore trailing newline */
