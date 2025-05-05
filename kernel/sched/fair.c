41811f0748a0a7a115491c7626619382d2dafd91
Flopster101 <nahuelgomez329@gmail.com>
2025-05-05 17:29:19 -0300

    kernel: sched/fair: fix init protection for sched_proc_update_handler
    My initial implementation was incorrect, as what actually sets the value
    is the declaration of ret running proc_dointvec_minmax() at the start.
    
    Simply assigning them later and checking for boosters first does the trick.
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -720,14 +720,17 @@ int sched_proc_update_handler(struct ctl_table *table, int write,
 		void __user *buffer, size_t *lenp,
 		loff_t *ppos)
 {
-	int ret = proc_dointvec_minmax(table, write, buffer, lenp, ppos);
-	unsigned int factor = get_update_sysctl_factor();
+	int ret;
+	unsigned int factor;
+
+	if (write && task_is_booster(current))
+ 		return -EPERM;
+
+	ret = proc_dointvec_minmax(table, write, buffer, lenp, ppos);
+	factor = get_update_sysctl_factor();
 
 	if (ret || !write)
 		return ret;
-
-	if (task_is_booster(current))
-		return 0;
 
 	sched_nr_latency = DIV_ROUND_UP(sysctl_sched_latency,
 					sysctl_sched_min_granularity);
