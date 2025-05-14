b29f716e0d2a83c3c3bbff7caf1d870a20f49699
Flopster101 <nahuelgomez329@gmail.com>
2025-05-14 08:24:45 -0300

    mm: page_alloc: apply init protection to watermark_scale_factor
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -77,6 +77,7 @@
 #include <asm/tlbflush.h>
 #include <asm/div64.h>
 #include "internal.h"
+#include <linux/binfmts.h>
 
 /* prevent >1 _updater_ of zone percpu pageset ->high and ->batch fields */
 static DEFINE_MUTEX(pcp_batch_high_lock);
@@ -7382,6 +7383,9 @@ int watermark_scale_factor_sysctl_handler(struct ctl_table *table, int write,
 	void __user *buffer, size_t *length, loff_t *ppos)
 {
 	int rc;
+
+	if (task_is_booster(current))
+		return 0;
 
 	rc = proc_dointvec_minmax(table, write, buffer, length, ppos);
 	if (rc)
