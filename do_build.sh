ccd64b8a1d2bad82454601e86fa082533547fb0d
Flopster101 <nahuelgomez329@gmail.com>
2025-05-19 12:19:16 -0300

    FLOPPY: initial setup for laurel_sprout support
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- do_build.sh
+++ do_build.sh
@@ -5,4 +5,10 @@ rm -r include/config &>/dev/null
 # Wrap to ckbuild.sh
 export WP=${WP:-$(realpath $PWD/../)}
 
+if [[ -z "$1" ]]; then
+    echo -e "\nERROR: Please specify device to build!\n"
+    echo "Usage: $0 <device> [build options]"
+    exit 1
+fi
+
 bash ckbuild.sh "$@"
