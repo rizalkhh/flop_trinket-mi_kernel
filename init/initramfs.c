4d0065707a466e1c8366f57e5a0b1feba9f04aa2
Flopster101 <nahuelgomez329@gmail.com>
2025-05-25 17:07:22 -0300

    fstabdt_disable: preference for legacy ROMs
    It makes more sense to default to legacy init ROMs if no param is passed.
    
    This inverts the logic of the flag.
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- init/initramfs.c
+++ init/initramfs.c
@@ -639,7 +639,7 @@ static int __init populate_rootfs(void)
 {
 	char *err;
 
-	if (!strstr(saved_command_line, "fstabdt_keep"))
+	if (strstr(saved_command_line, "tsinit"))
 		do_skip_initramfs = 0;
 
 	if (do_skip_initramfs) {
