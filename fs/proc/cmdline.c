7da5ad9ef2c5dc34669d10eb6d82015b8d86ee05
Flopster101 <nahuelgomez329@gmail.com>
2025-07-09 23:14:34 -0300

    Revert "[BACKPORT] fs: update to SUSFS v1.5.8"
    This reverts commit 777c56af17a41934391db3ba0567163e91f138ef.

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -7,10 +7,6 @@
 #include <asm/setup.h>
 #endif
 
-#ifdef CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
-extern int susfs_spoof_cmdline_or_bootconfig(struct seq_file *m);
-#endif
-
 static char proc_command_line[COMMAND_LINE_SIZE];
 
 #ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
@@ -31,12 +27,6 @@ static void proc_command_line_init(void) {
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
-#ifdef CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
-	if (!susfs_spoof_cmdline_or_bootconfig(m)) {
-		seq_putc(m, '\n');
-		return 0;
-	}
-#endif
 	seq_puts(m, proc_command_line);
 	seq_putc(m, '\n');
 	return 0;
