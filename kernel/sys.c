aa4063c80f833c597256d29b6beeeb783b693e3d
simonpunk <simonpunk2016@gmail.com>
2025-07-09 23:11:37 -0300

    [PATCH] treewide: Implement susfs v1.5.9
    https://github.com/TheSillyOk/kernel_ls_patches/blob/ca6b4bc354b6873b0153ce67a9aff62b8b57e522/susfs-1.5.9.patch
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>
    Co-authored-by: TheSillyOk <priv.ld@proton.me>

--- kernel/sys.c
+++ kernel/sys.c
@@ -1205,6 +1205,9 @@ static int override_version(struct new_utsname __user *name)
 #endif
 }
 
+#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
+extern void susfs_spoof_uname(struct new_utsname* tmp);
+#endif
 SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 {
 	struct new_utsname tmp;
@@ -1213,6 +1216,9 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
+#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
+	susfs_spoof_uname(&tmp);
+#endif
 	if (is_bpf_spoof_enabled()) {
 		if (current_uid().val == 0 &&
 			(!strncmp(current->comm, "bpfloader", 9) ||
