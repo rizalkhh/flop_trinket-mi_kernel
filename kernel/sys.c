0123a9cbbb86b7aa964229fbfb19046d5bbb9bf0
Flopster101 <nahuelgomez329@gmail.com>
2025-09-20 18:57:16 -0300

    Revert "[PATCH] treewide: Implement susfs v1.5.9"
    This reverts commit d901783af5e83d09998e9200c6830caf6624f223.

--- kernel/sys.c
+++ kernel/sys.c
@@ -1205,9 +1205,6 @@ static int override_version(struct new_utsname __user *name)
 #endif
 }
 
-#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
-extern void susfs_spoof_uname(struct new_utsname* tmp);
-#endif
 SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 {
 	struct new_utsname tmp;
@@ -1227,9 +1224,6 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 				current->comm, current->pid, tmp.release);
 		}
 	}
-#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
-	susfs_spoof_uname(&tmp);
-#endif
 	up_read(&uts_sem);
 
 	rcu_read_lock();
