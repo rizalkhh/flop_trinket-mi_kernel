2438eed1424e0c228612a0d55e3f85dc77d22b38
cvnertnc <148134890+cvnertnc@users.noreply.github.com>
2026-07-26 14:56:05 +0000

    xxksu: implement susfs v2.2.0 (De-inlined)
    Signed-off-by: cvnertnc <148134890+cvnertnc@users.noreply.github.com>
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- drivers/xxksu/kernel/ksu.c
+++ drivers/xxksu/kernel/ksu.c
@@ -148,6 +148,10 @@
 #include "hook/kp_ksud.c"
 #endif
 
+#ifdef CONFIG_KSU_SUSFS
+#include <linux/susfs.h>
+#endif // #ifdef CONFIG_KSU_SUSFS
+
 struct cred* ksu_cred;
 
 extern void ksu_supercalls_init();
@@ -245,6 +249,10 @@ static int __init kernelsu_init(void)
 	ksu_allowlist_init();
 
 	ksu_throne_tracker_init();
+
+#ifdef CONFIG_KSU_SUSFS
+    susfs_init();
+#endif // #ifdef CONFIG_KSU_SUSFS
 
 	ksu_ksud_init();
 
